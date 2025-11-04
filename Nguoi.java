803671bc3b4039db6cd29c27f3e0d30e89d06647
Nguyễn Hoàng ấn <bodowq@gmail.com>
2025-11-04 09:59:29 +0700

    lớp người và SV

---
+++ Nguoi.java
@@ -0,0 +1,11 @@
+import java.sql.Date;
+
+abstract class Nguoi {
+    protected String Cccd;
+    protected String HoTen;
+    protected Date NgaySinh;
+
+    abstract void Tinhluong();
+    abstract void XuatThongTin();
+    
+}
---
+++ SinhVien.java
@@ -0,0 +1,22 @@
+public class SinhVien extends Nguoi{
+    protected String MSSV;
+    protected String Khoa;
+    protected String Lop;
+    protected String SoTC;
+    protected String NamVH;
+    @Override
+    void Tinhluong(){ //Tính Học phí thay vì lương
+
+
+
+
+    }
+   void XuatThongTin()
+   {
+
+
+
+
+   }
+
+}
