149ca07f9fe693e8f2953e85ba6120828074accd
Nguyễn Hoàng ấn <bodowq@gmail.com>
2025-11-09 10:22:57 +0700

    xong trưởng khoa

--- ManageSchool/model/AdjunctProfessor.java
+++ ManageSchool/model/AdjunctProfessor.java
@@ -1,5 +1,22 @@
 package ManageSchool.model;
 public class AdjunctProfessor extends Lecturer {
-
+private String position;
+public String getPosition() {
+    return position;
+}
+public void setPosition(String position) {
+    this.position = position;
     
+}
+public AdjunctProfessor(String name, int age, String humanid, String dateOfBirth, String sex, String department,
+        String lecturerID, float teachingHours, String level, String position) {
+    super(name, age, humanid, dateOfBirth, sex, department, lecturerID, teachingHours, level);
+    this.position = position;
+}
+@Override
+double payroll(){
+return super.payroll() + super.payroll()*0.95;
+
+
+}
 }
